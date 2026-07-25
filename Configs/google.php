7795ba2adfe7329ec742c84f97cd8c7e17bbc241
Thai101-alt <buitranthai2005@gmail.com>
2026-07-25 21:40:05 +0700

    updaye

--- Configs/google.php
+++ Configs/google.php
@@ -13,6 +13,9 @@ $client->setClientSecret($_ENV['GOOGLE_CLIENT_SECRET']);
 
 $client->setRedirectUri($_ENV['GOOGLE_REDIRECT_URI']);
 
+$client->setPrompt('select_account');
+$client->setApprovalPrompt('force');
+
 $client->addScope("email");
 
-$client->addScope("profile");   
+$client->addScope("profile");
